9324227bc53bd8b4b94ce696eab9317b8f8c2318
SinaSystem <sinasystem47@gmail.com>
2020-10-27 23:31:52 +0330

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:carousel_slider_flutter/screen/slide_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SlideScreen()
+    );
+  }
+}
